e7b430516915d5758a337488a79708e32ca0f0a4
Sameer Faridi <s.faridi007@gmail.com>
2023-03-26 23:30:44 +0530

    late night JSON creation forcefully by (menupulated b anmol)

--- 26_03 diamond and API task/app.js
+++ 26_03 diamond and API task/app.js
@@ -1,6 +1,6 @@
 console.log("sameer")
 
-axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+axios.get('https://pokeapi.co/api/v2/pokemon?limit=30')
 .then(response =>{
     console.log(response.data);
 },err=>{
