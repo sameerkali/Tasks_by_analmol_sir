c04dacb1a5070a37e67f3519aed3e5a255aac165
Sameer Faridi <s.faridi007@gmail.com>
2023-04-05 17:31:52 +0530

    .

---
+++ 05_04_2023/reverseInLL.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class reverseInLL {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        size++;
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public static void main(String[] args) {
+        reverseInLL ll = new reverseInLL();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.addFirst(6);
+        ll.addFirst(7);
+        ll.addFirst(8);
+        ll.addFirst(9);
+        ll.addFirst(10);
+
+        ll.reverse();
+        ll.print();
+        
+    }
+}
