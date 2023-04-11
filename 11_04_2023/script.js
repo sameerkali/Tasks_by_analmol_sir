87299bcdfcd75e335c8323e8cc1bff98398b48e4
Sameer Faridi <s.faridi007@gmail.com>
2023-04-11 22:58:50 +0530

    v2

--- 11_04_2023/script.js
+++ 11_04_2023/script.js
@@ -10,13 +10,6 @@ const request = require('request')
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
-    // const jonar = req.body
-    // const url = `https://v2.jokeapi.dev/joke/any`
-    // console.log(url)
-    // request({ url: url }, (err, res) => {
-    //     const data = JSON.parse(res.body)
-    //     console.log(data.setup)
-    // })
 })
 
 
@@ -29,20 +22,20 @@ app.post("/", function(req, res){
         
             response.on("data", function(data){
                const jokes = JSON.parse(data)
-               console.log(jokes)
+
                const temp = jokes.setup
                const mazaak = jokes.joke
+               const delivery = jokes.delivery 
                if(temp == undefined){
                    res.write(`<h1> joke: ${ mazaak } </h1>`)
                }
                else{
                 res.write(`<h1> santa:  ${ temp } </h1>`)
-                res.write(`<h1> banta: ${ jokes.delivery } </h1>`)
+                res.write(`<h1> banta: ${ delivery } </h1>`)
                }
                res.send()
             })
         })
-
 })
 
 
