c5063ad6f0c95168a10293afd9640e78de0f3a09
Sameer Faridi <s.faridi007@gmail.com>
2023-07-12 14:00:41 +0530

    working 100% but looking not so good at that point

--- 11_July_2023/expense_app/app.js
+++ 11_July_2023/expense_app/app.js
@@ -1,54 +1,53 @@
+// This code imports the Express, body-parser, ejs, and mongoose modules.
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const port = process.env.PORT;
+// This creates a new Express application.
 const app = express();
+
+// This sets the view engine to EJS.
 app.set("view engine", "ejs");
 
+// This middleware tells Express to serve static files from the `public` directory.
 app.use(express.static("public"));
+
+// This middleware parses JSON and URL-encoded form data from requests.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// This connects to the MongoDB database.
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, console.log('database connected'));
 
+// This creates a new Mongoose schema for expenses.
 const expenseSchema = new mongoose.Schema({
   title: String,
   amount: Number,
   description: String
 });
 
+// This creates a new Mongoose model for expenses.
 const Expense = mongoose.model("Expense", expenseSchema);
 
+// This creates a new Mongoose schema for posts.
 const PostSchema = new mongoose.Schema({
   title: String,
   amount: Number,
   description: String
 });
 
-const Post = mongoose.model("Post", PostSchema);
+// This creates a new Mongoose model for posts.
+// const Post = mongoose.model("Post", PostSchema);
 
-const posts = [];
+// This is an array of posts.
+// const posts = [];
 
-// app.get("/", function (req, res) {
-//   Expense.find({})
-//     .then((expenses) => {
-//       res.render("home", {
-//         posts: posts,
-//         expenses: expenses
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-
-
+// This route renders the home page, which lists all of the expenses.
 app.get("/", function (req, res) {
   Expense.find({})
     .then((expenses) => {
@@ -58,19 +57,20 @@ app.get("/", function (req, res) {
     })
     .catch((err) => {
       console.log(err);
-      res.render("home", { expenses: [] }); // Render with empty expenses if an error occurs
-    });
+      res.render("home", { expenses: [] }); });
 });
 
-
-
+// This route renders the compose page, which allows users to create new expenses.
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.get("/post", function (req, res) {
-  res.render("post");
-});
 
+// This route renders the post page, which allows users to view and edit posts.
+// app.get("/post", function (req, res) {
+//   res.render("post");
+// });
+
+// This route creates a new expense.
 app.post("/compose", (req, res) => {
   const expense = new Expense({
     title: req.body.title,
@@ -88,120 +88,7 @@ app.post("/compose", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started on port 3000");
+// This starts the Express server on port.
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const ejs = require("ejs");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const app = express();
-// app.set("view engine", "ejs");
-
-// app.use(express.static("public"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// const expenseSchema = new mongoose.Schema({
-//   title: String,
-//   amount: Number,
-//   description: String
-// });
-
-// const Expense = mongoose.model("Expense", expenseSchema);
-
-// const PostSchema = new mongoose.Schema({
-//   title: String,
-//   amount: Number,
-//   description: String
-// });
-
-// const Post = mongoose.model("Post", PostSchema);
-
-// const posts = [];
-
-// app.get("/", function (req, res) {
-//   Post.find({})
-//     .then((posts) => {
-//       res.render("home", {
-//         posts: posts
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/compose", function (req, res) {
-//   res.render("compose");
-// });
-// app.get("/post", function (req, res) {
-//   res.render("post");
-// });
-// // app.post("/compose", (req, res) => {
-// //   const expense = new Expense({
-// //     title: req.body.title,
-// //     amount: req.body.amount,
-// //     description: req.body.description
-// //   });
-
-// //   expense
-// //     .save()
-// //     .then((posts) => {
-// //       res.render("home", {
-// //   posts: posts
-// // });
-// //     })
-// //     .catch((err) => {
-// //       console.log(err);
-// //     });
-// // });
-// app.post("/compose", (req, res) => {
-//   const post = new Post({
-//     title: req.body.title,
-//     amount: req.body.amount,
-//     description: req.body.description
-//   });
-
-//   post
-//     .save()
-//     .then(() => {
-//       posts.push(post);
-//       Post.find({})
-//         .then((posts) => {
-//           res.render("home", {
-//             posts: posts
-//           });
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log("Server started on port 3000");
-// });
